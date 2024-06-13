60df3aa9999a684d19973d27e8f43da97c1a011d
asil <9745019+asiill@users.noreply.github.com>
2024-06-13 12:25:43 -0400

    Add cart total

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import CartItemCard from "../components/CartItemCard";
 import { useCart } from "../context/CartContext.jsx";
+import itemData from "../assets/itemData.jsx";
 
 const Cart = () => {
     const { cartQuantity, items } = useCart();
     return (
         <section id="cart">
-            {cartQuantity > 0 && 
-                <div className="cart-items grid gap-12">
-                    {items.map(item => {
-                        return (
-                            <CartItemCard key={item.id} {...item}/>
-                        )
-                    })} 
+            {cartQuantity > 0 &&
+                <div className="flex flex-col gap-8">
+                    <div className="cart-items grid gap-12">
+                        {items.map(item => {
+                            return (
+                                <CartItemCard key={item.id} {...item}/>
+                            )
+                        })} 
+                    </div>
+                    <div className="border border-solid border-neutral-900"></div>
+                    <div className="flex flex-col gap-2 md:flex-row md:justify-around">
+                        <div className="flex gap-2 text-2xl">
+                            <div>Total:</div>
+                            <div>
+                                ${items.reduce((total, cartItem) => {
+                                    const item = itemData.find(i => i.id === cartItem.id);
+                                    return (total + (item?.price || 0) * cartItem.quantity);
+                                }, 0)
+                                }
+                            </div>
+                        </div>
+                        <button className="max-w-80">Checkout</button>
+                    </div>
                 </div>
             }
             {cartQuantity === 0 && 
