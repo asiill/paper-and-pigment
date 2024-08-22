805f5ed6fa61481e456c933e8da83bd4b7a0929c
asiill <asiill>
2024-08-21 22:12:48 -0400

    Import proptypes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import PropTypes from "prop-types";
+
 const Button = ({ title, handleClick }) => {
     return (
         <button onClick={handleClick} className="w-fit rounded-md p-2 text-md text-white text-bold bg-[#8C6577] transition-opacity hover:opacity-80">
